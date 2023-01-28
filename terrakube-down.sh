e38ec75e6d036aaaac449ee2fe5b4dc924279caa
Thomas Vincent <thomasvincent@gmail.com>
2023-01-28 00:39:41 -0800

    Update terrakube-down.sh

--- terrakube-down.sh
+++ terrakube-down.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
-root=$(pwd)
-config_file=$root/config/config.dev.tfvars
 
-cd $root/init
-terraform destroy -var-file $config-file
+set -eou pipefail
 
-cd $root
-terraform destroy -var-file $config_file
+root_dir=$(pwd)
+config_file="$root_dir/config/config.dev.tfvars"
+
+cd "$root_dir/init"
+
+terraform init -input=false
+terraform destroy -var-file "$config_file" -auto-approve
+
+cd "$root_dir"
+
+terraform init -input=false
+terraform destroy -var-file "$config_file" -auto-approve
